0769052009dc1b2610cd7637f5b89e4768e0ee99
Dorka845 <foldvari.dorka_2022b@bolyai-szakkozep.hu>
2026-09-20 17:27:26 +0200

    rgb szinezes

---
+++ 0916/functions.js
@@ -0,0 +1,41 @@
+function $(id){
+    return document.getElementById(id);
+}
+
+function rangeRGB(){    
+    const r = $('red-range').value;
+    const g = $('green-range').value;
+    const b = $('blue-range').value;
+    const a = $('alpha-range').value;
+
+    $('red-value').value = r;
+    $('green-value').value = g;
+    $('blue-value').value = b;
+    $('alpha-value').value = a;
+
+    document.getElementById('rgb-box').style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function valueRGB(){    
+    const r = $('red-value').value;
+    const g = $('green-value').value;
+    const b = $('blue-value').value;
+    const a = $('alpha-value').value;
+
+    $('red-range').value = r;
+    $('green-range').value = g;
+    $('blue-range').value = b;
+    $('alpha-range').value = a;
+
+    document.getElementById('rgb-box').style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+$('red-range').addEventListener('input', rangeRGB);
+$('green-range').addEventListener('input', rangeRGB);
+$('blue-range').addEventListener('input', rangeRGB);
+$('alpha-range').addEventListener('input', rangeRGB);
+
+$('red-value').addEventListener('input', valueRGB);
+$('green-value').addEventListener('input', valueRGB);
+$('blue-value').addEventListener('input', valueRGB);
+$('alpha-value').addEventListener('input', valueRGB);
